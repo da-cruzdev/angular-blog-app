9067ffc14eaf4891ae6217752ce418a67fb261a3
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-05 17:20:15 +0000

    show categories on interface

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Category } from 'src/app/models/category';
 import { CategoriesService } from 'src/app/services/categories.service';
 
@@ -8,14 +9,19 @@ import { CategoriesService } from 'src/app/services/categories.service';
   styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
+  categories!: Observable<{ category: string }[]>;
   constructor(private categoryService: CategoriesService) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.categories = this.categoryService.getData$();
+  }
 
-  onSumit(formData: any): void {
+  onSubmit(formData: any): void {
     let categoryData: Category = {
       category: formData.value.category,
     };
 
     this.categoryService.saveData(categoryData);
+
+    formData.reset();
   }
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+} from '@angular/fire/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +23,14 @@ export class CategoriesService {
       })
       .catch((err) => {
         console.log(err);
+        this.toastr.error(err);
       });
+  }
+
+  getData$() {
+    const collectionInstance = collection(this.firestore, 'categories');
+    return collectionData(collectionInstance) as Observable<
+      { category: string }[]
+    >;
   }
 }
