6a3a21c41639478d22725162bcd09e0ecda6d020
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-05 12:50:06 +0000

    create environment file for firebase config

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-blog-e5292',
+    appId: '1:1090766466926:web:f65934f80de19bbd8cdb7f',
+    storageBucket: 'ang-blog-e5292.appspot.com',
+    locationId: 'europe-west2',
+    apiKey: 'AIzaSyDHiJdcZg5L5_dlLU-ICjEBlqbZu4pGsYg',
+    authDomain: 'ang-blog-e5292.firebaseapp.com',
+    messagingSenderId: '1090766466926',
+  },
+  production: false,
+};
