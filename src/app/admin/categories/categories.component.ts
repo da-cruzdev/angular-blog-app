9ce8344b546ce48d289e7569feeecf88478ac390
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-05 12:51:11 +0000

    save category data in firestore database

--- src/app/admin/categories/categories.component.ts
+++ src/app/admin/categories/categories.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+  styleUrls: ['./categories.component.scss'],
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  constructor(private firestore: Firestore) {}
+  ngOnInit(): void {}
 
+  onSumit(formData: any): void {
+    let categoryData = {
+      category: formData.value.category,
+    };
+    const collectionData = collection(this.firestore, 'categories');
+    addDoc(collectionData, categoryData)
+      .then(() => {
+        console.log('Data saved successfully');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
