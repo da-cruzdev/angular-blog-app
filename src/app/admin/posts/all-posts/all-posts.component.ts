8742d179f6f6bc3136b24dcd62d888e686f352b6
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-07 09:30:17 +0000

    show data post in html component

--- src/app/admin/posts/all-posts/all-posts.component.ts
+++ src/app/admin/posts/all-posts/all-posts.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
-  styleUrls: ['./all-posts.component.scss']
+  styleUrls: ['./all-posts.component.scss'],
 })
-export class AllPostsComponent {
+export class AllPostsComponent implements OnInit {
+  posts!: Observable<any>;
 
+  constructor(private postService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postService.getData$();
+  }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
-import { Firestore, addDoc, collection } from '@angular/fire/firestore';
+import {
+  Firestore,
+  addDoc,
+  collection,
+  collectionData,
+} from '@angular/fire/firestore';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +17,8 @@ export class PostsService {
   constructor(
     private storage: AngularFireStorage,
     private firestore: Firestore,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private router: Router
   ) {}
 
   uploadImage(selectedImage: any, postData: any) {
@@ -35,6 +43,14 @@ export class PostsService {
     const collectionData = collection(this.firestore, 'posts');
     addDoc(collectionData, postData).then(() => {
       this.toastr.success('Post saved successfully...!!');
+      this.router.navigate(['/posts']);
     });
+  }
+
+  getData$() {
+    const collectionInstance = collection(this.firestore, 'posts');
+    return collectionData(collectionInstance, {
+      idField: 'id',
+    }) as Observable<any>;
   }
 }
