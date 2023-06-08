cb3822e5646199900aa57c282b8bf097572240d0
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-08 12:56:13 +0000

    add method to login firebase auth

--- src/app/admin/auth/login/login.component.ts
+++ src/app/admin/auth/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
+  ngOnInit(): void {}
+
+  onSubmit(formValue: any) {
+    this.authService.login(formValue.email, formValue.password);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    private auth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  login(email: any, password: any) {
+    this.auth
+      .signInWithEmailAndPassword(email, password)
+      .then((logRef) => {
+        this.toastr.success('Logged in successfully!!!!');
+        this.router.navigate(['/dashboard']);
+      })
+      .catch((err) => {
+        console.log(err);
+
+        this.toastr.error(err);
+      });
+  }
 }
